237e517f5ee5da75a7e4758a0692c9d8767694eb
leslie179 <144388669+leslie179@users.noreply.github.com>
2025-03-03 11:37:31 -0800

    Edit goldibite

--- .history/Components-goldiBite/About/index_20250303113659.js
+++ .history/Components-goldiBite/About/index_20250303113659.js
@@ -18,7 +18,7 @@ const About = () => {
             <div className={styles.rightColumn}>
                 <h2 className={styles.h2}>About the business</h2>
                 <p className={styles.p}>
-                GoldiBite is a travel-friendly app for tourists with dietary restrictions, ensuring safe dining and shopping. It features a barcode and menu scanner that translates ingredients and allergens into the user’s language. <br /> <br />The app also provides localized emergency resources, including hotlines and allergy protocols. With a focus on safety and accessibility, GoldiBite helps travelers make informed food choices anywhere.</p>
+                GoldiBite is a travel-friendly app for tourists with dietary restrictions, ensuring safe dining and shopping. It features a barcode and menu scanner that translates ingredients and allergens into the user’s language. <br /> <br />The app also provides localized emergency resources, including hotlines and allergy protocols. With a focus on safety and accessibility, Goldi Bites helps travelers make informed food choices anywhere.</p>
                 <h2 className={styles.h2}>Timeline</h2>
                 <p className={styles.p}>3.5 months</p>
                 <a href="https://www.figma.com/proto/QQNbhVtuF8ATLTZjSzYgQw/Screens?node-id=2601-16334&t=iMidhTxPcU0gOeAJ-0&scaling=min-zoom&content-scaling=fixed&page-id=2601%3A10685&starting-point-node-id=2601%3A16239&show-proto-sidebar=1" target="_blank" >
